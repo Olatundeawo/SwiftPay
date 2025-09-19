72b33c0d707436c54b0d735df0779216d16bcf0e
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-09-20 00:39:16 +0100

    added the middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import user from "./routes/user.js";
+import authMiddleware from "./middleware/authMiddleware.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const prisma = new PrismaClient();
 
 app.use("/", user(prisma));
+app.get("/dashboard", authMiddleware, (req, res) => {
+  res.json({
+    message: `Welcome to the ${req.user.role} dashboard, your email is ${req.user.email}`,
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
