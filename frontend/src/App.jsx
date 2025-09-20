8a14080c95efc2f421749e9a3a28162a32598faa
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-09-20 14:12:54 +0100

    called component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import Routing from "./component/routing";
-import Register from "./component/user";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Routing />
-      {/* <Register /> */}
+      <div>
+        <Routing />
+        {/* <Register /> */}
+      </div>
     </>
   );
 }
