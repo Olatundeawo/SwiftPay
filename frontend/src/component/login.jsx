d0f007a7ccc888e581560df498f869426faa1af3
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-09-20 14:16:44 +0100

    created and tested the compoent for routing

--- frontend/src/component/login.jsx
+++ frontend/src/component/login.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const Login = () => {
   return (
     <div>
-      <h2>This is the login page</h2>
+      <h2 className="text-red-500">This is the login page</h2>
       <p>You must provide a valid credential to access this page.</p>
     </div>
   );
