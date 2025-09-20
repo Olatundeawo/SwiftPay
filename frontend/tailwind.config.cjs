873e5c61377f0a56d19f496c62d6739dde2e8f96
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-09-20 14:17:03 +0100

    modifty

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,12 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.cjs
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
