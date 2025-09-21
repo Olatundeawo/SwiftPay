41e8a8b154477ec31ea83d067debb78e7cb22928
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-09-21 18:39:13 +0100

    created the dashboard for the mechant

---
+++ frontend/src/component/merchantDashboar.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+const Merchant = () => {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const data = await fetch("http://localhost:3000/dashboard", {
+          method: "GET",
+
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        const response = await data.json();
+
+        setUser(response.user);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <p>This is the Merchant Dashboard, welcome {user.name}.</p>
+      <p> your balance is {user.walletBalance}</p>
+    </div>
+  );
+};
+
+export default Merchant;
