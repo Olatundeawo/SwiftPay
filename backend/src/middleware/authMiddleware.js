a10822ead574210dd586e00b772ddd3d9b3dddce
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-09-21 18:41:44 +0100

    fixed the error by adding a return

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -6,14 +6,15 @@ export default function (req, res, next) {
   const token = req.header("Authorization");
 
   if (!token) {
-    res.status(401).json({ success: false, error: "Not Autorize" });
+    return res.status(401).json({ success: false, error: "Not Autorize" });
   }
 
   try {
     const verified = jwt.verify(token.split(" ")[1], SECRET);
     req.user = verified;
-    next();
+    return next();
   } catch (err) {
+    console.error({ Error: err });
     return res.status(400).json({ success: false, error: err.message });
   }
 }
