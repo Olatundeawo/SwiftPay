fbfb20d8682093c4ad7fe3ff32b900f0c3ee7c65
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-09-25 16:58:16 +0100

    modify

--- frontend/src/component/merchantDashboar.jsx
+++ frontend/src/component/merchantDashboar.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 
 const Merchant = () => {
   const [user, setUser] = useState("");
